6026276b39b7c5815d9a405279c5e17a6c19fe4d
yugmani <yugmani@yahoo.com>
2022-04-27 00:17:31 -0700

    ui for the keys and characters

--- index.js
+++ index.js
@@ -19,7 +19,35 @@ console.log(codeOfCharacter('p')); //112
 console.log(codeOfCharacter('g')); //103
 console.log(codeOfCharacter('G')); //71
 
+const keyArray = [71, 103, 112];
 
+function displayCodeCharacters(arr) {
+  let html = '<ul>';
+  arr.forEach((item) => {
+    html += `<li>The character for key ${item} is ${characterCode(item)}.</li>`;
+  });
+
+  html += '</ul>';
+  document.querySelector('.charcodes').innerHTML = html;
+}
+
+displayCodeCharacters(keyArray);
+
+const charArray = ['G', 'p', 'g'];
+
+function displayCharacterCode(arr) {
+  let html = '<ul>';
+  arr.forEach((item) => {
+    html += `<li>The key for character ${item} is ${codeOfCharacter(
+      item
+    )}.</li>`;
+  });
+
+  html += '</ul>';
+  document.querySelector('.codeschar').innerHTML = html;
+}
+
+displayCharacterCode(charArray);
 
 //function to return the name of key pressed in the input area.
 const keyCode = document.getElementById('keyCode');
