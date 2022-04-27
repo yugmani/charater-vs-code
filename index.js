c27b87244e96ee560cd0cb70ceca20a489455b0d
yugmani <yugmani@yahoo.com>
2022-04-26 23:56:02 -0700

    conversion of keys into characters and vice versa

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 //Function to return the character of the given code
 function characterCode(num) {
   let str = String.fromCharCode(num);
@@ -5,12 +7,31 @@ function characterCode(num) {
 }
 
 console.log(characterCode(71)); //G
+console.log(characterCode(103)); //g
+console.log(characterCode(112)); //p
+
+function codeOfCharacter(char) {
+  let value = char.charCodeAt(char);
+  return value;
+}
+
+console.log(codeOfCharacter('p')); //112
+console.log(codeOfCharacter('g')); //103
+console.log(codeOfCharacter('G')); //71
+
+
 
 //function to return the name of key pressed in the input area.
 const keyCode = document.getElementById('keyCode');
+const myKey = document.getElementById('myKey');
 
 keyCode.addEventListener('keydown', (e) => {
   if (!e.repeat) {
     console.log(`${e.key}`);
+    myKey.innerHTML = `You just pressed the key: <span id="ekey">${e.key}</span>`;
   }
 });
+
+// Enter -->when return key is pressed
+// a --> when 'a' key is pressed
+// NumLock -->when 'Num Lock' key pressed
